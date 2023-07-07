7f4f1b23fd8ab299224fba509d463c1e41cef819
Rogério Farias Otto <rogerio.otto@l2code.com.br>
2023-07-06 23:19:27 -0300

    refactor: something

--- src/components/SidebarIcon/index.tsx
+++ src/components/SidebarIcon/index.tsx
@@ -11,22 +11,11 @@ type sidebarIconProps = {
 export default function SidebarIcon(props: sidebarIconProps) {
 
     return (
-        <>
-        {!props.isOpen ? (
-            <a href={props.href}>
-                <div className="SidebarIconContainer">
-                    <img src={props.icon} alt='sidebar icon' />
-                </div>
-            </a>
-        ) : (
-            <a href={props.href}>
-                <div className="SidebarIconContainer">
-                    <img src={props.icon} alt='sidebar icon' />
-                    <p className="SidebarText">{props.text}</p>
-                </div>
-            </a>
-        )}
-        </>
-        
+        <a href={props.href}>
+            <div className="SidebarIconContainer">
+                <img src={props.icon} alt='sidebar icon' />
+                {props.isOpen && (<p className="SidebarText">{props.text}</p>)}
+            </div>
+        </a>
     )
 }
